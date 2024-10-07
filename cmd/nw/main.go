204c86c90cfb0f42a82ad3009191022e41fa457d
Abdoul Sy <dreescan@gmail.com>
2024-10-07 03:15:39 +0200

    initial router

---
+++ cmd/nw/main.go
@@ -0,0 +1,470 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+type File struct {
+	Path    string
+	Content []byte
+}
+
+func writeFileWithCheck(file File) error {
+	if err := os.WriteFile(file.Path, file.Content, os.ModePerm); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	cmd := NewCommand()
+	cmd.Register(
+		runProject,
+		installProject,
+		generateProject,
+		generateConcept,
+		generateView,
+		generateHandler,
+	)
+
+	cmd.Execute()
+}
+
+func runProject() *cobra.Command {
+	return &cobra.Command{
+		Use:     "run",
+		Example: "t0 run",
+		Short:   "Run Tndr0cean development server",
+		Run: func(cmd *cobra.Command, args []string) {
+			if _, err := os.Stat("cmd/main.go"); err != nil {
+				fmt.Println("not in Tndr0cean app root: cmd/main.go not found")
+				return
+			}
+			if err := exec.Command("t1", "generate").Run(); err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			if err := exec.Command("go", "run", "cmd/main.go").Run(); err != nil {
+				fmt.Println(err)
+			}
+		},
+	}
+}
+
+func installProject() *cobra.Command {
+	return &cobra.Command{
+		Use:     "install",
+		Aliases: []string{"i"},
+		Example: "nw install",
+		Short:   "Install project's dependency",
+		Run: func(cmd *cobra.Command, args []string) {
+			start := time.Now()
+			fmt.Println("installing project...")
+			if err := exec.Command("go", "get", "github.com/senforsce/tndr0cean@latest").Run(); err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			if err := exec.Command("go", "get", "github.com/senforsce/tndr").Run(); err != nil {
+				fmt.Println(err)
+				return
+			}
+			if err := exec.Command("t1", "generate").Run(); err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			fmt.Printf("done installing project in %v\n", time.Since(start))
+		},
+	}
+}
+
+func generateProject() *cobra.Command {
+	return &cobra.Command{
+		Use:     "new",
+		Example: "nw new hello-world",
+		Short:   "Create new nw project",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) == 0 {
+				fmt.Println("invalid arguments")
+				return
+			}
+
+			name := args[0]
+
+			fmt.Println("creating new nw project:", name)
+			if err := os.Mkdir(name, os.ModePerm); err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			files := []File{
+				// setup directory
+				{Path: name + "/Concepts/", Content: nil},
+				{Path: name + "/Concepts/Example", Content: nil},
+				{Path: name + "/Concepts/Example/layout", Content: nil},
+				{Path: name + "/Concepts/Example/HelloWorld", Content: nil},
+				{Path: name + "/Concepts/Example/handler", Content: nil},
+				{Path: name + "/cmd", Content: nil},
+				{Path: name + "/public", Content: nil},
+
+				// setup files
+				{Path: name + "/go.mod", Content: writeGoModContents(name)},
+				{Path: name + "/.air.toml", Content: writeAirTomlContents()},
+				{Path: name + "/.env", Content: writeEnvFileContents()},
+				{Path: name + "/.gitignore", Content: writeGitignore()},
+				{Path: name + "/public/app.css", Content: []byte("")},
+				{Path: name + "/cmd/main.go", Content: writeMainContents(name)},
+				{Path: name + "/Concepts/Example/handler/hello.go", Content: writeHandlerContent(name)},
+				{Path: name + "/Concepts/Example/layout/base.t1", Content: writeBaseLayoutContent()},
+				{Path: name + "/Concepts/Example/HelloWorld/view.t1", Content: writeViewContent(name)},
+			}
+
+			errors := []error{}
+			for _, file := range files {
+				if file.Content == nil {
+					if err := os.Mkdir(file.Path, os.ModePerm); err != nil {
+						errors = append(errors, err)
+					}
+				} else {
+					if err := writeFileWithCheck(file); err != nil {
+						errors = append(errors, err)
+					}
+				}
+			}
+
+			if len(errors) != 0 {
+				fmt.Println("Tndr0cean encountered errors during file initialization:", errors)
+				return
+			}
+		},
+	}
+}
+
+func generateConcept() *cobra.Command {
+	return &cobra.Command{
+		Use:     "concept",
+		Example: "Tndr0cean concept user",
+		Short:   "Generate new concept with ontology",
+		Run: func(cmd *cobra.Command, args []string) {
+			// TODO:
+		},
+	}
+}
+
+func generateView() *cobra.Command {
+	return &cobra.Command{
+		Use:     "view",
+		Example: "Tndr0cean view user",
+		Short:   "Generate new view",
+		Run: func(cmd *cobra.Command, args []string) {
+			// TODO:
+
+		},
+	}
+}
+
+func generateHandler() *cobra.Command {
+	return &cobra.Command{
+		Use:     "handler",
+		Example: "Tndr0cean handler home",
+		Short:   "Generate new handler",
+		Run: func(cmd *cobra.Command, args []string) {
+			// TODO:
+
+		},
+	}
+}
+
+func writeEnvFileContents() []byte {
+	return []byte(`
+T0_HTTP_LISTEN_ADDR=:3000
+
+T0_SQL_DB_NAME=
+T0_SQL_DB_USER=
+T0_SQL_DB_PASSWORD=
+T0_SQL_DB_HOST=
+T0_SQL_DB_PORT=
+
+T0_O8_DB_NAME=
+T0_O8_DB_USER=
+T0_O8_DB_PASSWORD=
+T0_O8_DB_HOST=
+T0_O8_DB_PORT=
+`)
+}
+
+func writeDBFileContents() []byte {
+	c := `
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"sync"
+)
+
+var (
+	conn *sql.DB
+	once sync.Once
+)
+
+type config struct {
+	Hostname string
+	Username string
+	Password string
+	Port     string
+	DBName   string
+}
+
+func (c *config) Connect() (*sql.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		c.Username,
+		c.Password,
+		c.Hostname,
+		c.Port,
+		c.DBName,
+	)
+
+	log.Println("Creating database connection using DSN:", dsn)
+
+	conn, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Println("Error opening database connection:", err)
+		return nil, err
+	}
+
+	log.Println("Pinging database to verify connection...")
+
+	err = conn.Ping()
+	if err != nil {
+		log.Println("Error pinging database:", err)
+		return nil, err
+	}
+
+	log.Println("Database connection established successfully.")
+
+	return conn, nil
+}
+
+func OpenConnection() {
+	log.Println("Attempting to open database connection...")
+
+	mysql := config{
+		Hostname: os.Getenv("NW_SQL_DB_HOST"),
+		Username: os.Getenv("NW_SQL_DB_USER"),
+		Password: os.Getenv("NW_SQL_DB_PASSWORD"),
+		Port:     os.Getenv("NW_SQL_DB_PORT"),
+		DBName:   os.Getenv("NW_SQL_DB_NAME"),
+	}
+
+	once.Do(func() {
+		log.Println("Creating database connection...")
+
+		db, err := mysql.Connect()
+		if err != nil {
+			log.Println("Error connecting to database:", err)
+			return
+		}
+
+		log.Println("Storing database connection...")
+		conn = db
+	})
+
+	log.Println("Database connection opened.")
+}
+
+func GetConnection() *sql.DB {
+	if conn == nil {
+		OpenConnection()
+	}
+
+	return conn
+}
+
+type Config struct {
+	DB *sql.DB
+}
+
+func NewConfig() *Config {
+	/**
+	 * Open connection
+	 */
+
+	log.Println("Attempting to retrieve database connection...")
+	db := GetConnection()
+	log.Printf("Database connection retrieved: %v\n", db)
+
+	if db == nil {
+		log.Println("ERROR: Database connection failed.")
+		return nil
+	}
+
+	config := Config{
+		DB: db,
+	}
+
+	return &config
+}
+
+`
+	return []byte(c)
+}
+
+func writeMainContents(mod string) []byte {
+	c := fmt.Sprintf(`
+package main
+
+import (
+	"log"
+	"github.com/senforsce/tndr0cean"
+	"%s/handler"
+)
+
+func main() {
+	app := nw.New()
+	app.Get("/", handler.HandleHelloIndex)
+	log.Fatal(app.Start())
+}
+`, mod)
+	return []byte(c)
+}
+
+func writeGoModContents(mod string) []byte {
+	buf := strings.Builder{}
+	buf.WriteString("module " + mod)
+	buf.WriteString("\n")
+	buf.WriteString("\n")
+	buf.WriteString("go 1.21.6")
+	return []byte(buf.String())
+}
+
+func writeAirTomlContents() []byte {
+	c := `
+root = "."
+testdata_dir = "testdata"
+tmp_dir = ".build"
+
+[build]
+  args_bin = []
+  bin = "./.build/main"
+  cmd = "t1 generate && go build -o ./.build/main ./cmd"
+  delay = 1000
+  exclude_dir = ["assets", ".build", "vendor", "testdata"]
+  exclude_file = []
+  exclude_regex = ["_test.go", "_t1.go"]
+  exclude_unchanged = false
+  follow_symlink = false
+  full_bin = ""
+  include_dir = []
+  include_ext = ["go", "tpl", "tmpl", "html", "templ"]
+  include_file = []
+  kill_delay = "0s"
+  log = "build-errors.log"
+  poll = false
+  poll_interval = 0
+  post_cmd = []
+  pre_cmd = []
+  rerun = false
+  rerun_delay = 500
+  send_interrupt = false
+  stop_on_error = false
+
+[color]
+  app = ""
+  build = "yellow"
+  main = "magenta"
+  runner = "green"
+  watcher = "cyan"
+
+[log]
+  main_only = false
+  time = false
+
+[misc]
+  clean_on_exit = false
+
+[screen]
+  clear_on_rebuild = false
+  keep_scroll = true
+
+`
+	return []byte(c)
+}
+
+func writeViewContent(mod string) []byte {
+	c := fmt.Sprintf(`
+package helloWorld
+
+import (
+	"%s/view/layout"
+)
+
+t1 Index() {
+	@layout.Base() {
+		<h1>hello there</h1>
+	}
+}
+`, mod)
+
+	return []byte(c)
+}
+
+func writeHandlerContent(mod string) []byte {
+	c := fmt.Sprintf(`
+package handler
+
+import (
+	"github.com/senforsce/Tndr0cean"
+	"%s/view/hello"
+)
+
+func HandleHelloIndex(c *router.Context) error {
+	return c.Render(hello.Index())
+}
+`, mod)
+
+	return []byte(c)
+}
+
+func writeGitignore() []byte {
+	c := `
+bin
+.build
+.env
+`
+	return []byte(c)
+}
+
+func writeBaseLayoutContent() []byte {
+	c := `
+package layout
+
+t1 Base() {
+	<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="utf-8"/>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+			<title>Slick Application</title>
+
+			<script src="https://unpkg.com/htmx.org@1.9.9" defer></script>
+		</head>
+		<body>
+			{ children... }
+		</body>
+	</html>
+}
+`
+	return []byte(c)
+}
---
+++ router/router.go
@@ -0,0 +1,227 @@
+package router
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+	"github.com/julienschmidt/httprouter"
+	"github.com/senforsce/tndr"
+)
+
+type Plug func(Handler) Handler
+
+type Handler func(c *Context) error
+
+type ErrorHandler func(error, *Context) error
+
+type Context struct {
+	Response http.ResponseWriter
+	Request  *http.Request
+	ctx      context.Context
+	params   httprouter.Params
+}
+
+func newContext(w http.ResponseWriter, r *http.Request, params httprouter.Params) *Context {
+	return &Context{
+		Response: w,
+		Request:  r,
+		ctx:      context.Background(),
+		params:   params,
+	}
+}
+
+func (c *Context) Param(name string) string {
+	return c.params.ByName(name)
+}
+
+func (c *Context) Query(name string) string {
+	return c.Request.URL.Query().Get(name)
+}
+
+func (c *Context) FormValue(name string) string {
+	return c.Request.FormValue(name)
+}
+
+func (c *Context) Render(component t1.Component) error {
+	return component.Render(c.ctx, c.Response)
+}
+
+func (c *Context) Redirect(url string, code int) error {
+	if code < http.StatusMultipleChoices || code > http.StatusTemporaryRedirect {
+		return errors.New("invalid redirect code")
+	}
+	http.Redirect(c.Response, c.Request, url, code)
+	return nil
+}
+
+func (c *Context) JSON(status int, v any) error {
+	c.Response.Header().Set("Content-Type", "application/json")
+	c.Response.WriteHeader(status)
+	return json.NewDecoder(c.Request.Body).Decode(&v)
+}
+
+func (c *Context) Text(status int, t string) error {
+	c.Response.Header().Set("Content-Type", "text/plain")
+	c.Response.WriteHeader(status)
+	_, err := c.Response.Write([]byte(t))
+	return err
+}
+
+func (c *Context) Set(key string, value any) {
+	c.ctx = context.WithValue(c.ctx, key, value)
+}
+
+func (c *Context) Get(key string) any {
+	res := c.ctx.Value(key)
+
+	if res != nil {
+		return res
+	}
+
+	return key
+}
+
+type Tndr0cean struct {
+	ErrorHandler ErrorHandler
+	router       *httprouter.Router
+	plugs        []Plug
+	Global       GlobalStorage
+}
+
+func New() *Tndr0cean {
+	return &Tndr0cean{
+		router:       httprouter.New(),
+		ErrorHandler: defaultErrorHandler,
+		Global: GlobalStorage{
+			Data: make(map[string]string),
+		},
+	}
+}
+
+// initialized by the first start
+type GlobalStorage struct {
+	Data map[string]string
+}
+
+func (t0 *Tndr0cean) S(key string, value string) {
+	t0.Global.Data[key] = value
+}
+
+func (t0 *Tndr0cean) G(key string) string {
+	return t0.Global.Data[key]
+}
+
+type methodNotAllowedHandler struct {
+	handler Handler
+}
+
+func (h methodNotAllowedHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := newContext(w, r, httprouter.Params{})
+	h.handler(ctx)
+}
+
+func (s *Tndr0cean) MethodNotAllowed(h Handler) {
+	s.router.MethodNotAllowed = methodNotAllowedHandler{h}
+}
+
+func (s *Tndr0cean) Plug(plugs ...Plug) {
+	s.plugs = append(s.plugs, plugs...)
+}
+
+func (s *Tndr0cean) Start() error {
+	if err := godotenv.Load(); err != nil {
+		return err
+	}
+
+	// Retrieve and sanitize listen address from env
+	listenAddr := os.Getenv("T0_HTTP_LISTEN_ADDR")
+	listenAddr = strings.TrimSpace(listenAddr)
+
+	// If listen address is not set, use default host and port
+	if listenAddr == "" {
+		listenAddr = ":3000"
+	}
+
+	// Print the URL where the app is running
+	browsableURL := listenAddr
+	if strings.HasPrefix(browsableURL, ":") {
+		browsableURL = "localhost" + browsableURL
+
+	}
+
+	path, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+
+	filePath := fmt.Sprintf("%s/static", path)
+
+	s.router.ServeFiles("/static/*filepath", http.Dir(filePath))
+
+	fmt.Printf("Tndr0cean app running at http://%s\n", browsableURL)
+
+	fmt.Printf("Tndr0cean serving files at %s%s\n file path is %s\n", path, "/static/", filePath)
+
+	// Start the HTTP server
+	return http.ListenAndServe(listenAddr, s.router)
+}
+
+func (s *Tndr0cean) add(method, path string, h Handler, plugs ...Plug) {
+	s.router.Handle(method, path, s.makeHTTPRouterHandle(h, plugs...))
+}
+
+func (s *Tndr0cean) Get(path string, h Handler, plugs ...Plug) {
+	s.add("GET", path, h, plugs...)
+}
+
+func (s *Tndr0cean) Post(path string, h Handler, plugs ...Plug) {
+	s.add("POST", path, h, plugs...)
+}
+
+func (s *Tndr0cean) Put(path string, h Handler, plugs ...Plug) {
+	s.add("PUT", path, h, plugs...)
+}
+
+func (s *Tndr0cean) Delete(path string, h Handler, plugs ...Plug) {
+	s.add("DELETE", path, h, plugs...)
+}
+
+func (s *Tndr0cean) Head(path string, h Handler, plugs ...Plug) {
+	s.add("HEAD", path, h, plugs...)
+}
+
+func (s *Tndr0cean) Options(path string, h Handler, plugs ...Plug) {
+	s.add("OPTIONS", path, h, plugs...)
+}
+
+func (s *Tndr0cean) makeHTTPRouterHandle(h Handler, plugs ...Plug) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+		ctx := newContext(w, r, params)
+		for j, v := range s.Global.Data {
+			ctx.Set(j, v)
+		}
+		for i := len(plugs) - 1; i >= 0; i-- {
+			h = plugs[i](h)
+		}
+		for i := len(s.plugs) - 1; i >= 0; i-- {
+			h = s.plugs[i](h)
+		}
+		if err := h(ctx); err != nil {
+			// todo: handle the error from the error handler huh?
+			s.ErrorHandler(err, ctx)
+		}
+	}
+}
+
+func defaultErrorHandler(err error, c *Context) error {
+	slog.Error("error", "err", err)
+	return nil
+}
